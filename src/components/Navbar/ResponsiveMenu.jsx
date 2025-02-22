6609ac75c14fd116ee1125c404236e57f78cad88
Vinishireis <nishimuravinicius28@gmail.com>
2025-02-21 22:01:24 -0300

    Atualização tela ReponsiveMenu

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import { Link } from "react-router-dom"; // Importe o Link
 
 const ResponsiveMenu = ({ isOpen }) => {
   return (
@@ -14,11 +15,47 @@ const ResponsiveMenu = ({ isOpen }) => {
         >
           <div className="text-xl font-semibold uppercase bg-primary text-black py-10 m-6 rounded-3xl">
             <ul className="flex flex-col justify-center items-center gap-10">
-              <li>Inicio</li>
-              <li>Sobre</li>
-              <li>Serviço</li>
-              <li>Galeria</li>
-              <li>Contato</li>              
+              <li>
+                <Link to="/" className="hover:text-orange-500 transition-colors duration-200">
+                  Inicio
+                </Link>
+              </li>
+              <li>
+                <Link to="/sobre" className="hover:text-orange-500 transition-colors duration-200">
+                  Sobre
+                </Link>
+              </li>
+              <li>
+                <Link to="/servico" className="hover:text-orange-500 transition-colors duration-200">
+                  Serviço
+                </Link>
+              </li>
+              <li>
+                <Link to="/galeria" className="hover:text-orange-500 transition-colors duration-200">
+                  Galeria
+                </Link>
+              </li>
+              <li>
+                <Link to="/contato" className="hover:text-orange-500 transition-colors duration-200">
+                  Contato
+                </Link>
+              </li>
+              
+              <li>
+                <Link to="/blog" className="hover:text-orange-500 transition-colors duration-200">
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link to="/auth" className="hover:text-orange-500 transition-colors duration-200">
+                  Login
+                </Link> 
+              </li>
+              <li>
+                <Link to="/register" className="hover:text-orange-500 transition-colors duration-200">
+                  Inscreva-se
+                </Link>
+              </li>
             </ul>
           </div>
         </motion.div>
